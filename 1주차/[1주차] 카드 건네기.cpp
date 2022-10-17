e95419333dc0eb01ee9b89477f8ca57dba1d5650
조민규/연구원/Cyber Security Engineering Unit(mingyu.jo@lge.com) <mingyu.jo@lge.com>
2022-10-17 16:35:52 +0900

    week1

---
+++ 1주차/[1주차] 카드 건네기.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+#define MAXN (100)
+int N;
+int sol[MAXN + 10];
+
+void InputData(){
+	cin >> N;
+}
+
+void OutputData(){
+	for (int i=0; i<N; i++){
+		cout << sol[i] << "\n";
+	}
+}
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	InputData();//입력
+	//여기서부터 작성
+	OutputData();//출력
+	return 0;
+}
