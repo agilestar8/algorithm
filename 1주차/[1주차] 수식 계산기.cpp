e46fe18fd76e83ddbfbba404ffcdf890efdddba4
조민규/연구원/Cyber Security Engineering Unit(mingyu.jo@lge.com) <mingyu.jo@lge.com>
2022-10-17 14:41:36 +0900

    1week

--- 1주차/[1주차] 수식 계산기.cpp
+++ 1주차/[1주차] 수식 계산기.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
+#include <stack>
 using namespace std;
+
+// 사칙연산 수식을 입력 받아 그 수식의 결과를 출력하는 프로그램을 작성하시오.
+// 수식은 수와 +, -, *, /의 연산자로 구성되어 있으며 각 수는 10 이하의 양의 정수이다.
+// 나눗셈의 처리결과는 몫만을 취하도록 한다.
 
 #define MAX (20)
 int N;
@@ -25,12 +30,47 @@ int main(){
 
 	//여기서부터 작성
 
-    scanf("%d", &N);
-    
+    stack<int> arr;
+	int result = 0;
+	int temp = 0;
 
+	arr.push(M[0]);
 
+	for(int i=1;i<N;i++){			
+		switch (op[i])
+		{
+		case '+':
+			arr.push(M[i]);			
+			break;
+		
+		case '-':
+			arr.push(-M[i]);
+			break;
 
-	cout << ans << "\n";
+		case '*':
+			temp = arr.top() * M[i];
+			arr.pop();
+			arr.push(temp);
+			break;
+
+		case '/':
+			temp = arr.top() / M[i];
+			arr.pop();
+			arr.push(temp);
+			break;
+
+		} // switch loop end
+	} // for loop end
+
+	while (true){
+		if (arr.empty()){
+			break;
+		}
+		result += arr.top();
+		arr.pop(); 
+	}
+
+	cout << result << "\n";
 
 	return 0;
 }
